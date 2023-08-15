58e0ffd81adc62e5ebeab77f15609cd4f28ab5b2
max-vakulenko <kampbell07071997@gmail.com>
2023-08-15 19:00:49 +0300

    added a photo upload to the background

---
+++ js/consts/api_keys.js
@@ -0,0 +1,5 @@
+const apiWatherKey = '4e2c6bccf22c53a9ea562bbdd1bc2491';
+const apiUnsplashKey = 'WYN05gVlchzbVT30ntXnZ6fYFiXjhkwCDtunv0n1rQw'
+const apiUnsplashSecretKey = 'bWU7fqKK7j4LygIvrdIQ3uV_PoyU5Z_XO3s5bxBeyTo'
+
+export { apiWatherKey, apiUnsplashKey, apiUnsplashSecretKey }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
+import { apiWatherKey, apiUnsplashKey } from './consts/api_keys.js'
+
 const temp = document.querySelector('#temp');
 const wind = document.querySelector('#wind');
 const humidity = document.querySelector('#humidity');
 const cloudy = document.querySelector('#cloudy');
 const searchInput = document.querySelector('#searchInput');
-const apiWatherKey = '4e2c6bccf22c53a9ea562bbdd1bc2491';
+const body = document.getElementsByTagName('body')[0];
 
 searchInput.oninput = _.debounce(async event => {
 	let request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${apiWatherKey}&units=metric&lang=ua`)
+	let requestImages = await fetch(`https://api.unsplash.com/search/photos?page=1&per_page=1&query=${event.target.value}&client_id=${apiUnsplashKey}&lang=uk&orientation=landscape`)
 	let response = await request.json()
+	let responseImages = await requestImages.json()
 	event.target.value = ''
-	displayWeather(response)
+	console.log(responseImages);
+	displayWeather(response, responseImages)
 }, 1000)
 
-function displayWeather(data) {
+function displayWeather(data, images) {
 	temp.innerText = Math.round(data.main.temp) + '°C';
 	wind.innerText = data.wind.speed + 'км/год';
 	humidity.innerText = data.main.humidity + '%';
 	cloudy.innerText = data.weather[0].description;
+	body.style.backgroundImage = `url(${images.results[0].urls.full})`
 }
